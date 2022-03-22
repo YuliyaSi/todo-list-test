146281c1dbc9b6c0f903f1f32b46e5df9aedb69d
Yuliya Sidarenka <yulka1102@gmail.com>
2022-03-22 12:04:18 +0100

    realize add task in list functionality

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Col, Container, FormControl, InputGroup, ListGroup, Row} from "react-bootstrap";
 import {RiAddLine} from "react-icons/ri";
 import "./style.css"
 import {AiFillDelete} from "react-icons/ai";
 
 export const TodoApp = () => {
+    const [todo, setTodo] = useState('');
+    const [list, setList] = useState([]);
+
+    const addToList = () => {
+      setList(list => [...list, todo]);
+      setTodo('');
+    }
 
     return (
         <Container fluid={true} className='todoapp__container'>
@@ -13,8 +20,13 @@ export const TodoApp = () => {
                     <InputGroup className="mb-3 xs-12 md-6 lg-2">
                     <FormControl
                         placeholder="Type something..."
+                        value={todo}
+                        onChange={event => setTodo(event.target.value)}
                     />
-                    <Button variant="outline-secondary">
+                    <Button
+                        variant="outline-secondary"
+                        onClick={addToList}
+                    >
                         <RiAddLine/>
                     </Button>
                 </InputGroup>
@@ -23,12 +35,13 @@ export const TodoApp = () => {
             <Row className='justify-content-center align-content-center'>
                 <Col sm={8} md={4} lg={3}>
                     <ListGroup>
-                        <ListGroup.Item className='todoapp__item'>
-                            <span>text</span>
+                        {list.map(item => <ListGroup.Item className='todoapp__item'>
+                            <span>{item}</span>
                             <Button variant="outline-secondary">
                                 <AiFillDelete/>
                             </Button>
-                        </ListGroup.Item>
+                        </ListGroup.Item>)}
+
                     </ListGroup>
                 </Col>
             </Row>
